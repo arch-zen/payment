1993818d8bf523216ea202a9c78052791ddbb2bc
王旭东 <wangxudong@ymatou.com>
2016-05-12 14:22:13 +0800

    添加测试套件

---
+++ payment-test/src/test/java/com/ymatou/payment/test/PaymentTestSuit.java
@@ -0,0 +1,19 @@
+/**
+ * (C) Copyright 2016 Ymatou (http://www.ymatou.com/).
+ *
+ * All rights reserved.
+ */
+package com.ymatou.payment.test;
+
+
+
+import org.junit.runner.RunWith;
+
+import com.googlecode.junittoolbox.SuiteClasses;
+import com.googlecode.junittoolbox.WildcardPatternSuite;
+
+@RunWith(WildcardPatternSuite.class)
+@SuiteClasses({"**/*Test.class",
+        "!*BaseTest.class"})
+public class PaymentTestSuit {
+}
