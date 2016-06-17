9b91df20e694c53e9c88bf6f2821f1dc5e6d7438
王旭东 <wangxudong@ymatou.com>
2016-06-17 17:46:16 +0800

    add trading refund notify mock

--- payment-test/src/test/java/com/ymatou/payment/test/integration/service/ymatou/RefundCallbackServiceTest.java
+++ payment-test/src/test/java/com/ymatou/payment/test/integration/service/ymatou/RefundCallbackServiceTest.java
@@ -6,6 +6,7 @@ package com.ymatou.payment.test.integration.service.ymatou;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashMap;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -43,4 +44,49 @@ public class RefundCallbackServiceTest extends RestBaseTest {
 
         Assert.assertEquals(true, isSuccess);
     }
+
+    @Test
+    public void doServiceTestWithMock() throws IOException {
+        RefundCallbackRequest request = new RefundCallbackRequest();
+        request.setActualRefundAmount(new BigDecimal(0.01));
+        request.setAuditor("system");
+        request.setOptType(10);
+        request.setOrderID(111642639);
+        request.setPassAuditTime(new Date());
+        request.setRequiredRefundAmount(new BigDecimal(0.01));
+        request.setThirdPartyName("Alipay");
+        request.setThirdPartyTradingNo("2016053021001004580212614604");
+        request.setTradeNo("13912628");
+        request.setIsFastRefund(true);
+
+        HashMap<String, String> mockHeader = buildMockHeader();
+        mockHeader.put("MockResult-Trading-Status", "OK");
+        boolean isSuccess = refundCallbackService.doService(request, mockHeader);
+
+        Assert.assertEquals(true, isSuccess);
+    }
+
+
+    @Test
+    public void doServiceTestWithMock2() throws IOException {
+        RefundCallbackRequest request = new RefundCallbackRequest();
+        request.setActualRefundAmount(new BigDecimal(0.01));
+        request.setAuditor("system");
+        request.setOptType(10);
+        request.setOrderID(111642639);
+        request.setPassAuditTime(new Date());
+        request.setRequiredRefundAmount(new BigDecimal(0.01));
+        request.setThirdPartyName("Alipay");
+        request.setThirdPartyTradingNo("2016053021001004580212614604");
+        request.setTradeNo("13912628");
+        request.setIsFastRefund(true);
+
+        HashMap<String, String> mockHeader = buildMockHeader();
+        mockHeader.put("MockResult-Trading-Status", "FAILD");
+        boolean isSuccess = refundCallbackService.doService(request, mockHeader);
+
+        Assert.assertEquals(false, isSuccess);
+    }
+
+
 }
