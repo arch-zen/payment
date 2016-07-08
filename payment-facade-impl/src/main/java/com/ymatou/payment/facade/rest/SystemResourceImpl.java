e8189b3a999a3cea2a45f297cea66fb8af23ad12
qianmin <qianmin@ymatou.com>
2016-07-08 11:30:30 +0800

    execute return code

--- payment-facade-impl/src/main/java/com/ymatou/payment/facade/rest/SystemResourceImpl.java
+++ payment-facade-impl/src/main/java/com/ymatou/payment/facade/rest/SystemResourceImpl.java
@@ -35,7 +35,7 @@ public class SystemResourceImpl implements SystemResource {
     @Path("/version")
     @Override
     public String version() {
-        return "2016-07-07.2";
+        return "2016-07-08.1";
     }
 
 
