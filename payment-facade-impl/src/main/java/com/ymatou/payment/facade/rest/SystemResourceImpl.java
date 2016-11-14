a4892b60fff8fdea50e08c48da17da53439fcbb5
王旭东 <wangxudong@ymatou.com>
2016-11-14 14:29:34 +0800

    fix alipay wap validate merchant id

--- payment-facade-impl/src/main/java/com/ymatou/payment/facade/rest/SystemResourceImpl.java
+++ payment-facade-impl/src/main/java/com/ymatou/payment/facade/rest/SystemResourceImpl.java
@@ -35,7 +35,7 @@ public class SystemResourceImpl implements SystemResource {
     @Path("/version")
     @Override
     public String version() {
-        return "2016-11-14.1 validate merchant id";
+        return "2016-11-14.2 validate merchant id";
     }
 
 
